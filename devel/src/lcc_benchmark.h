d214025bd6940aeb3d2851452a8dba186efdd135
Andras Strausz <astrausz@ault02.cscs.ch>
2021-01-11 18:43:32 +0100

    reordering, intersection

---
+++ devel/src/lcc_benchmark.h
@@ -0,0 +1,7 @@
+// #define DEGBSD_REORDER 1
+#define OMP_SCHEDULING "omp parallel for schedule(static)"
+#define RANDOM_REORDER 1
+#define ROW_MEM_SIZE "268435456"
+#define ROW_HT_ENTRIES "8000"
+#define COL_MEM_SIZE "41943040"
+#define COL_HT_ENTRIES "8000"
